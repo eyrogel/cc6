ccfe5f1d01916069dbf2f1be05dc273ae69a3fe1
eyrogel <acu6621@students.uc-bcf.edu.ph>
2023-11-17 21:58:08 +0800

    Initial Commit

---
+++ app/src/main/java/com/app/androidarcore/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.app.androidarcore
+
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.assets.RenderableSource
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var renderableSource: RenderableSource
+    private lateinit var renderable: ModelRenderable
+    private lateinit var fragment: ArFragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Load the 3D Model Resources
+        renderableSource = RenderableSource.builder()
+            .setSource(
+                this,
+                Uri.parse("path/to/model.glb"), // or model.glb if you chose binary glTF
+                RenderableSource.SourceType.GLB
+            )
+            .setRecenterMode(RenderableSource.RecenterMode.ROOT)
+            .build()
+
+        // Build 3D Model Renderable
+        ModelRenderable.builder()
+            .setSource(this, renderableSource)
+            .build()
+            .thenAccept { modelRenderable ->
+                renderable = modelRenderable
+            }
+            .exceptionally { _ ->
+                // Handle errors here
+                null
+            }
+
+        // Add a listener to show AR model
+        fragment = supportFragmentManager.findFragmentById(R.id.ux_fragment) as ArFragment
+
+        fragment.setOnTapArPlaneListener { hitResult, _, _ ->
+            val anchorNode = AnchorNode(hitResult.createAnchor())
+            anchorNode.setParent(fragment.arSceneView.scene)
+            val transformableNode = TransformableNode(fragment.transformationSystem)
+            transformableNode.renderable = renderable
+            transformableNode.setParent(anchorNode)
+            transformableNode.select()
+        }
+    }
+}
